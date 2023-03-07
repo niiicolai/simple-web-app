d5b99356d25e0d03b45be836426a04a74efcc726
Nicolai Andersen <niiicolai@gmail.com>
2023-03-07 19:11:19 +0100

    add SSL support

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 
 const app = express();
 
@@ -11,8 +14,23 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
+// SSL configuration
+let sslOptions = null;
+if (process.env.NODE_ENV === 'production') {
+  sslOptions = {
+    key: fs.readFileSync('/ssl/key.pem'),
+    cert: fs.readFileSync('/ssl/cert.pem')
+  };
+}
+
 // Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+if (sslOptions) {
+  https.createServer(sslOptions, app).listen(PORT, () => {
+    console.log(`HTTPS server listening on port ${PORT}`);
+  });
+} else {
+  http.createServer(app).listen(PORT, () => {
+    console.log(`HTTP server listening on port ${PORT}`);
+  });
+}
