1866561488fc42897495fe082a5675bcc59cb183
Nicolai Andersen <niiicolai@gmail.com>
2023-03-07 19:50:48 +0100

    redirect http to https

--- server.js
+++ server.js
@@ -11,26 +11,33 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 // Fallback route for all other requests
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 // SSL configuration
 let sslOptions = null;
 if (process.env.NODE_ENV === 'production') {
-  sslOptions = {
-    key: fs.readFileSync('/ssl/key.pem'),
-    cert: fs.readFileSync('/ssl/cert.pem')
-  };
+    sslOptions = {
+        key: fs.readFileSync('/ssl/key.pem'),
+        cert: fs.readFileSync('/ssl/cert.pem')
+    };
 }
 
 // Start the server
-const PORT = process.env.PORT || 3000;
 if (sslOptions) {
-  https.createServer(sslOptions, app).listen(PORT, () => {
-    console.log(`HTTPS server listening on port ${PORT}`);
-  });
+    const HTTP_PORT = 80;
+    http.createServer((req, res) => {
+        res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+        res.end();
+    }).listen(HTTP_PORT);
+
+    const HTTPS_PORT = 443;
+    https.createServer(sslOptions, app).listen(HTTPS_PORT, () => {
+        console.log(`HTTPS server listening on port ${HTTPS_PORT}`);
+    });
 } else {
-  http.createServer(app).listen(PORT, () => {
-    console.log(`HTTP server listening on port ${PORT}`);
-  });
+    const DEV_PORT = 3000;
+    http.createServer(app).listen(DEV_PORT, () => {
+        console.log(`HTTP server listening on port ${DEV_PORT}`);
+    });
 }
